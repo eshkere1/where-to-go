d5e210aa898f2b7ad811ee482c92c940a7c6a309
Pavel <piv17032009@gmail.com>
2026-03-29 12:56:40 +0300

    Add Place model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
-# Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название")
+    description_short = models.TextField(verbose_name="Краткое описание", blank=True)
+    description_long = tinymce_models.HTMLField(verbose_name="Полное описание", blank=True)
+    longitude = models.DecimalField(verbose_name="Долгота", max_digits=17, decimal_places=14)
+    latitude = models.DecimalField(verbose_name="Широта", max_digits=17, decimal_places=14)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+
