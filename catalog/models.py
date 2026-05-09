bf7444a27444dddcfe9b11e6a51642fc4118b9a2
Pavel <piv17032009@gmail.com>
2026-05-09 13:58:01 +0300

    поменял значение на False

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название")
     description_short = models.TextField(verbose_name="Краткое описание", blank=True)
-    description_long = HTMLField(verbose_name="Полное описание", blank=True, null=True)
+    description_long = HTMLField(verbose_name="Полное описание", blank=True, null=False)
     longitude = models.DecimalField(verbose_name="Долгота", max_digits=17, decimal_places=14)
     latitude = models.DecimalField(verbose_name="Широта", max_digits=17, decimal_places=14)
 
